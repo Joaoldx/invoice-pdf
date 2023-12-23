4e2369b9c7d76c9c934759cbfb32c71c50fb12ec
João Domingos <j13domingos@gmail.com>
2023-12-23 18:00:24 -0300

    build: Configurado lint, prettier, e criado a primeira rota da aplicação

---
+++ src/shared/http/server.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (request: Request, response: Response) => {
+  return response.send('Primeira rota');
+});
+
+app.listen(3333, () => {
+  console.log('Servidor inicializou');
+});
